0923396b6bb5ddfb65aec076b64a86cf6d0a3926
maxim_novikov_88 <m.v.novikov@gmail.com>
2019-03-24 20:43:58 +0300

    try to find build problem

--- src/js/components/common/libs/DynamicImport.js
+++ src/js/components/common/libs/DynamicImport.js
@@ -2,24 +2,45 @@ import React, { useState, useEffect } from "react";
 
 export default function DynamicImport({path, props, isShowLoading = true}){
 
-    const LoadingModule = () => (isShowLoading ? <div className="content-container pt-20">Loading module...</div> : null);
     const [ Component, setComponent] = useState({module: null});
     let isCancelled = false;
 
     useEffect(() => {
         import(`../../${path}`)
-            .then(module => {
-                if(!isCancelled){
-                    setComponent({ module : module.default})
-                }
-            });
+          .then(module => {
+              if(!isCancelled){
+                  setComponent({ module : module.default})
+              }
+          });
 
         return () => {
             isCancelled = true;
         }
     }, []);
 
-    if(!Component.module) { return <LoadingModule/> }
+    if(!Component.module) return <div className="content-container pt-20">Loading module...</div>;
     const DynamicComponent = Component.module;
     return <DynamicComponent {...props}/>;
+
+
+    // const LoadingModule = () => (isShowLoading ? <div className="content-container pt-20">Loading module...</div> : null);
+    // const [ Component, setComponent] = useState({module: null});
+    // let isCancelled = false;
+    //
+    // useEffect(() => {
+    //     import(`../../${path}`)
+    //         .then(module => {
+    //             if(!isCancelled){
+    //                 setComponent({ module : module.default})
+    //             }
+    //         });
+    //
+    //     return () => {
+    //         isCancelled = true;
+    //     }
+    // }, []);
+    //
+    // if(!Component.module) { return <LoadingModule/> }
+    // const DynamicComponent = Component.module;
+    // return <DynamicComponent {...props}/>;
 }
