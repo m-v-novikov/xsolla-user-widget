943942d002e4f9c0c5c741038c2aff4ff8cf1323
maxim_novikov_88 <m.v.novikov@gmail.com>
2019-03-24 20:15:29 +0300

    babel options for prod

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,13 @@ module.exports = (env) => {
           exclude: /node_modules/,
           use:{
             loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/plugin-syntax-dynamic-import"
+              ]
+            }
           }
         },
         {
